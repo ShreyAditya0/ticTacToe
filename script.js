0585dfa68ac0201f91e29aed7a377b1f0fffd408
ShreyAditya0 <ishushrey03@gmail.com>
2024-07-02 09:49:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const cells = document.querySelectorAll('.cell');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart');
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let isGameOver = false;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
