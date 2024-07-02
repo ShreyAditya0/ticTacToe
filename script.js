4344d33b9f95bfddc2e2555148aeee840ee07b68
ShreyAditya0 <ishushrey03@gmail.com>
2024-07-02 13:01:14 +0530

    completed

--- script.js
+++ script.js
@@ -15,3 +15,54 @@ const winningCombinations = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+cells.forEach(cell => {
+    cell.addEventListener('click', handleClick);
+});
+
+restartButton.addEventListener('click', restartGame);
+
+function handleClick(event) {
+    const index = event.target.dataset.index;
+    if (board[index] === '' && !isGameOver) {
+        board[index] = currentPlayer;
+        event.target.textContent = currentPlayer;
+        if (checkWin()) {
+            message.textContent = `${currentPlayer} wins!`;
+            isGameOver = true;
+        } else if (board.every(cell => cell !== '')) {
+            message.textContent = "It's a tie!";
+            isGameOver = true;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
+
+function checkWin() {
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            highlightWinningCells(combination);
+            return true;
+        }
+    }
+    return false;
+}
+
+function highlightWinningCells(combination) {
+    combination.forEach(index => {
+        cells[index].classList.add('winner');
+    });
+}
+
+function restartGame() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    cells.forEach(cell => {
+        cell.textContent = '';
+        cell.classList.remove('winner');
+    });
+    currentPlayer = 'X';
+    isGameOver = false;
+    message.textContent = '';
+}
